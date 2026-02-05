4ec23d792cb5d7aeab79327b8bfa2b19c517e0f2
shaikshalearndevops <learndevops2102@gmail.com>
2026-02-05 16:41:22 +0530

    variable examples

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,7 +2,10 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then
+if [ $USERID -eq 0 ]; then
+    echo "You have root user access"
+    
+    else
     echo "Please run this script with root user access"
     exit 1
 fi
@@ -10,11 +13,12 @@ fi
 echo "Installing Nginx"
 dnf install nginx -y
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
+    echo "Installing Nginx ...SUCCESS "
+    
+else
     echo "Installing Nginx ... FAILURE"
     exit 1
-else
-    echo "Installing Nginx ... SUCCESS"
 fi
 
 dnf install mysql -y
