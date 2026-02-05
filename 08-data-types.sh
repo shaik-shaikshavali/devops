ffa6b7484a325d62400ddd73ff925f109cf98267
shaikshalearndevops <learndevops2102@gmail.com>
2026-02-05 15:20:56 +0530

    variable examples

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -3,14 +3,14 @@
 NUM1=100
 NUM2=sivakumar
 
-SUM=$(($NUM1+$NUM2))
+Sum=$(($NUM1+$NUM2))
 
-echo "Sum is: $SUM"
+echo "Sum is: $Sum"
 
 # Array
 Fruits=("Apple" "Banana" "Pomo")
 
 echo "Fruits are: ${Fruits[@]}"
-echo "First Fruit is: ${FRUITS[0]}"
+echo "First Fruit is: ${Fruits[0]}"
 echo "Second Fruit is: ${FRUITS[1]}"
 echo "Third Fruit is: ${FRUITS[2]}"
