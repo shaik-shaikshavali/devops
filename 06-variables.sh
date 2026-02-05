1d8a862431d2a48c867f265c65a1f1ef446ee07a
shaikshalearndevops <learndevops2102@gmail.com>
2026-02-05 13:00:22 +0530

    dynamic variables

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 
-# START_TIME=$(date +%s)
+echo "Script executed at: $START_TIME"
 
-# echo "Script executed at: $START_TIME"
-
-# sleep 10
-
-# END_TIME=$(date +%s)
-# TOTAL_TIME=$(($END_TIME-$START_TIME))
-
-# echo "Script executed in: $TOTAL_TIME seconds"
-
-TIME_TAKEN=$(date)
 sleep 10
-echo "Total time: $TIME_TAKEN"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
+echo "Script executed in: $TOTAL_TIME seconds"
